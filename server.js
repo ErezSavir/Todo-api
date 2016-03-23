a0a86443fc50d1b084f7357e5e9428c4e7b82d2b
Erez Savir <Savirerez@gmail.com>
2016-03-23 15:13:55 +0200

    refactor

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 
 var app = express();
@@ -21,14 +22,8 @@ app.get('/todos',function (req,res) {
 // GET /todos/:id
 app.get('/todos/:id',function (req,res) {
 	var todoID = parseInt(req.params.id,10);
-	var matchedTodo;
 
-	todos.forEach(function (todo) {
-		if (todoID === todo.id){
-			matchedTodo = todo;
-		}
-	});
-
+	var matchedTodo = _.findWhere(todos,{id: todoID});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -39,14 +34,15 @@ app.get('/todos/:id',function (req,res) {
 
 // POST /todos
 app.post('/todos',function (req,res) {
-	var body = req.body;
+	var body = _.pick(req.body,'completed','description');
 
-	//Add id field
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
 	body.id = todoNextId++;
-	
+	body.description = body.description.trim();
 	todos.push(body);
-	
-	
 	res.json(body);
 });
 
