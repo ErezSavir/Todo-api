6d0c2fa0c5fe6bcd2f5cd559835dec6c96e12eb4
Erez Savir <Savirerez@gmail.com>
2016-03-27 13:56:25 +0300

    Added variable playground

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+// 	name: 'Erez',
+// 	age:21
+// };
+
+// function updatePerson(obj) {
+// 	// obg = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+var grades = [15,88];
+
+function addGrades(grades) {
+	grades.push(55);
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
